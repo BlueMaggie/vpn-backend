712f0133e3c0c554eee78777c8757d117c7c98b5
BlueMaggie <473211890@qq.com>
2023-03-02 23:54:02 +0800

    new

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,5 +14,5 @@ function loadRouter(dirname) {
     })
 }
 router.redirect('/','/index.html',302)//重定向到静态页面
-router.redirect('/GetNewLink','/index.html',302)
+router.redirect('/GetNewLink','/',302)//兼容以前的链接
 module.exports = router
