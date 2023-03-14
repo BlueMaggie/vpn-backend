b2d625ba52395efaaf78c0ddb9db2da6e12ac791
BlueMaggie <473211890@qq.com>
2023-03-15 00:46:52 +0800

    new

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -20,7 +20,7 @@ class UserController {
         }
         var useragent = ctx.request.header['user-agent']
         //若为空则禁止登陆
-        if (useragent == null||useragent=='') {
+        if (useragent == null || useragent == '') {
             return ctx.app.emit('error', useragentUnknow, ctx)
         }
         var secret = calcMD5Str(useragent + JWT_SECRET)
@@ -28,7 +28,9 @@ class UserController {
         if (! await isClientIDExist(uid, secret))//如果该客户端未登录
         {
             var clients = await getAllClientID(uid)
-            deleteClientID(uid, clients[0].clientMD5)
+            if (clients.length == process.env.MAX_CLIENT_NUM) {
+                deleteClientID(uid, clients[0].clientMD5)
+            }
             createClientID(uid, secret)
         }
         ctx.cookies.set("token", jwt.sign(args, secret, { //token令牌
@@ -36,8 +38,8 @@ class UserController {
         }), {
             maxAge: 60 * 60 * 1000 * 24 * 7
         })
-        ctx.cookies.set("id",uid,{
-            maxAge:60*60*1000*24*7
+        ctx.cookies.set("id", uid, {
+            maxAge: 60 * 60 * 1000 * 24 * 7
         })
     }
 }
